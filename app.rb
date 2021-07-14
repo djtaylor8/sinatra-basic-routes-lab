88362ee8a2b8b7e0e7551728803aa00b67b80a16
DJ Taylor <dtaylor88@gmail.com>
2021-07-14 14:04:29 -0700

    commit tree 231d5848c821b20a7d475b12b2605c0c9ae70ebb

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is DJ Taylor"
+    end
+
+    get '/hometown' do 
+        "My hometown is Dallas, Texas"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is King of Carrot Flowers"
+    end
 end
